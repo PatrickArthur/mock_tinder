3c9e27348af14a07e52c27fba2f2a9ffeda5de21
Patrick <pa@arthurfamily.ws>
2019-02-21 11:50:43 -0500

    testing why migration didn't update in heroku

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -5,6 +5,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :fog
+  #test
   storage :fog
 
   # Override the directory where uploaded files will be stored.
